8b9aa77082a0030e995c7ce58fc4949f9e3b07b2
matay <matay@matanya-NPMHHI8>
2019-12-16 23:29:29 +0200

    readME javadoc to complexFunction and function_GUI

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -1,21 +1,30 @@
 package Ex1;
 import Ex1.Polynom;
 import Ex1.Monom;
+
 public class ComplexFunction implements complex_function{
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	function right;
 	function left;
 	Operation op;
 
+	/**
+	 *initial the tree from string
+	 *the string can be monom or polymom or complexFunc
+	 */
 	@Override
 	public function initFromString(String s) {
+		
 		// TODO Auto-generated method stub
 		ComplexFunction cf1=new ComplexFunction();
 		return FromString(s,cf1);
 	}
+	/**
+	 * @param s 
+	 * @param cf1
+	 * @return
+	 */
 	private function FromString(String s,ComplexFunction cf1) {
 		//  
 		String leftside="";
@@ -55,18 +64,32 @@ public class ComplexFunction implements complex_function{
 		return cf1;
 	}
 	
+	/**
+	 * constructor none as a default
+	 */
 	private ComplexFunction() {
+		
 		this.right=this.left=null;
 		this.op=Operation.None;
 	}
+	/**
+	 * @param func can be monom or polymom or complexFunc
+	 */
 	public ComplexFunction(function func) {
+		
 		// TODO Auto-generated constructor stub
 		if(func instanceof Monom) func= new Polynom((Monom)func);
 		this.left=func.copy();
 		this.right=null;
 		this.op= Operation.None;
 	}
+	/**
+	 * @param op one of this sing :Plus, Times, Divid, Max, Min, Comp , None, Error
+	 * @param left can be monom or polymom or complexFunc
+	 * @param right can be monom or polymom or complexFunc
+	 */
 	public ComplexFunction(Operation op, function left, function right) {
+		
 		// TODO Auto-generated constructor stub
 		if(left instanceof Monom) left= new Polynom((Monom)left);
 		if(right instanceof Monom) right= new Polynom((Monom)right);
@@ -74,10 +97,22 @@ public class ComplexFunction implements complex_function{
 		this.left = left.copy();
 		this.right=right.copy(); 
 	}
+	/**
+	 * @param op one of this sing :Plus, Times, Divid, Max, Min, Comp , None, Error
+	 * @param left can be monom or polymom or complexFunc
+	 * @param right can be monom or polymom or complexFunc
+	 */
 	public ComplexFunction(String op, function left, function right) {
+		
 		 this(getop(op),left,right);
 	}
+	/**
+	 * 
+	 * @param left can be monom or polymom or complexFunc
+	 * the other are null
+	 */
 	public ComplexFunction(String left) {
+		
 		// TODO Auto-generated constructor stub
 		if(!left.contains(",")) {
 			this.left= new Polynom(left);
@@ -90,8 +125,14 @@ public class ComplexFunction implements complex_function{
 		this.right=null;
 		this.op= Operation.None;
 	}
-	public ComplexFunction(String string, String left, String right) {
-		// TODO Auto-generated constructor stub
+	/**
+	 * @param op one of this sing :Plus, Times, Divid, Max, Min, Comp , None, Error
+	 * @param left can be monom or polymom or complexFunc
+	 * @param right can be monom or polymom or complexFunc
+	 */
+	public ComplexFunction(String op, String left, String right) {
+		
+		
 		if(!left.contains(",")) {
 			this.left= new Polynom(left);
 		}
@@ -106,11 +147,16 @@ public class ComplexFunction implements complex_function{
 			this.right=new ComplexFunction();
 			this.right=this.right.initFromString(right);
 		}
-		this.op= getop(string);
+		this.op= getop(op);
 	}
 
+	
+	/**
+	 *calculate the func at point 'x'
+	 */
 	@Override
 	public double f(double x) {
+		
 		// TODO Auto-generated method stub
 		if(this.left==null) return 0;
 		else if(this.op==Operation.None) return this.left.f(x);
@@ -139,7 +185,13 @@ public class ComplexFunction implements complex_function{
  */
 
 
+	/**
+	 * @param s the func you want to check
+	 * @param indexOf index of charater at the string 
+	 * @return
+	 */
 	private int getmed(String s, int indexOf) {
+		
 	// TODO Auto-generated method stub
 		int i =s.length()-2;
 		int count=0;
@@ -152,7 +204,13 @@ public class ComplexFunction implements complex_function{
 		}
 	return 0;
 }
+	
+	/**
+	 * @param oper :Plus, Times, Divid, Max, Min, Comp , None, Error
+	 * @return operator
+	 */
 	public static Operation getop(String oper) {
+		
 	// TODO Auto-generated method stub
 		oper=oper.toLowerCase();
 		if(oper.equalsIgnoreCase( "plus") )
@@ -172,8 +230,12 @@ public class ComplexFunction implements complex_function{
 		else 
 			return Operation.Error;
 }
+	/**
+	 * deep copy
+	 */
 	@Override
 	public function copy() {
+		
 		// TODO Auto-generated method stub
 		ComplexFunction cf= new ComplexFunction();
 		cf.left= this.left.copy();
@@ -183,9 +245,14 @@ if(this.right!= null)
 return cf;
 	}
 
+	/**
+	 * f1 can be monom or polymom or complexFunc 
+	 * f1 push to right node
+	 * and plus to operator
+	 */
 	@Override
 	public void plus(function f1) {
-		// TODO Auto-generated method stub
+		
 		if(this.op==Operation.None) {
 			this.op=Operation.Plus;
 			this.right=f1;
@@ -197,7 +264,11 @@ return cf;
 		
 		
 	}
-
+	/**
+	 * f1 can be monom or polymom or complexFunc 
+	 * f1 push to right node
+	 * and mul to operator
+	 */
 	@Override
 	public void mul(function f1) {
 		if(this.op==Operation.None) {
@@ -209,7 +280,11 @@ return cf;
 		this.right=f1.copy();
 		this.op=Operation.Times;	
 	}
-
+	/**
+	 * f1 can be monom or polymom or complexFunc 
+	 * f1 push to right node
+	 * and div to operator
+	 */
 	@Override
 	public void div(function f1) {
 		if(this.op==Operation.None) {
@@ -221,7 +296,11 @@ return cf;
 		this.right=f1.copy();
 		this.op=Operation.Divid;
 	}
-
+	/**
+	 * f1 can be monom or polymom or complexFunc 
+	 * f1 push to right node
+	 * and max to operator
+	 */
 	@Override
 	public void max(function f1) {
 		if(this.op==Operation.None) {
@@ -233,7 +312,11 @@ return cf;
 		this.right=f1;
 		this.op=Operation.Max;
 	}
-
+	/**
+	 * f1 can be monom or polymom or complexFunc 
+	 * f1 push to right node
+	 * and min to operator
+	 */
 	@Override
 	public void min(function f1) {
 		if(this.op==Operation.None) {
@@ -245,7 +328,11 @@ return cf;
 		this.right=f1.copy();
 		this.op=Operation.Min;
 	}
-
+	/**
+	 * f1 can be monom or polymom or complexFunc 
+	 * f1 push to right node
+	 * and comp to operator
+	 */
 	@Override
 	public void comp(function f1) {
 		if(this.op==Operation.None) {
@@ -258,12 +345,18 @@ return cf;
 		this.op=Operation.Comp;
 	}
 
+	/**
+	 *return left Node
+	 */
 	@Override
 	public function left() {
+		
 		// TODO Auto-generated method stub
 		return this.left;
 	}
-
+	/**
+	 *return right Node
+	 */
 	@Override
 	public function right() {
 		// TODO Auto-generated method stub
@@ -271,27 +364,41 @@ return cf;
 		else
 		return this.right();
 	}
-
+	/**
+	 *return operator
+	 */
 	@Override
 	public Operation getOp() {
 		// TODO Auto-generated method stub
 		return this.op;
 	}
+	/**
+	 *convert the object to string
+	 */
 	@Override
 	public String toString() {
+		
 		StringBuilder sb = new StringBuilder();
 		toString(sb);
 		
 		return sb.toString();
 		
 	}
+	/**
+	 * @param sb string builder from build string efficiently
+	 */
 	private void toString(StringBuilder sb) {
+		
 		// TODO Auto-generated method stub
 		 sb.append(""+this.op+"("+this.left+","+this.right+")");
 		return ;
 	}
 	
+	/**
+	 * return true if the object are logicly equals ti f1
+	 */
 	public boolean equals (Object f1) {
+		
 		boolean definedA=true;
 		boolean definedB=true;	
 		if(f1 instanceof function) {
